0533a4247f2e5a7cf8073d8d1b199f4b9a5a2404
Anubhav Dhruv <156454635+Anubhav505@users.noreply.github.com>
2024-08-01 21:43:03 +0530

    Delete public directory

--- public/js/reply.js
+++
@@ -1,8 +0,0 @@
-function showButton(event) {
-  event.stopPropagation();
-  document.getElementById("welcomeButton").style.display = "block";
-}
-
-document.addEventListener("click", function () {
-  document.getElementById("welcomeButton").style.display = "none";
-});
